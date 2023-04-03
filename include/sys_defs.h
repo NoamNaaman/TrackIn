826674001bf97b24ab2b5491d6eae4803828234a
nn1024 <n.noam@yahoo.com>
2023-04-03 13:15:57 +0300

    added new sys_defs.h file to git

---
+++ include/sys_defs.h
@@ -0,0 +1,9 @@
+
+// function prototypes
+
+
+extern void ZMOD_setup(void) ;
+extern void ZMOD_loop(void) ;
+extern void Serial_init(void);
+extern void SHTC1_init(void);
+extern void SHTC1_loop(void);
