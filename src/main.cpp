0240a93fafae352a1516dae93c46f7d3f3f23e0d
gidimiz <gidimiz@gmail.com>
2023-05-01 22:01:32 +0300

    Working 4450 with SD Card

--- src/main.cpp
+++ src/main.cpp
@@ -21,8 +21,25 @@
 #include <zmod4xxx.h>
 #include <zmod4xxx_hal.h>
 #include <oaq_2nd_gen.h>
-// start sequencer defines
 
+// Start SD card defines
+#include <SPI.h>
+#include <SD.h>
+
+#define SD_CS_PIN PA8
+#define BUTTON_PIN PB5
+#define LED_PIN PA0
+#define DATA_SAVE_INTERVAL 10000 // in milliseconds
+#define FILE_MAX_SIZE 3600 // in number of data records
+#define DATA_REG_SIZE 3 // number of data registers
+float data[DATA_REG_SIZE];
+File dataFile;
+unsigned long lastDataSaveTime = 0;
+int fileIndex = 0;
+int dataRecordCount = 0;
+bool isSavingData = false;
+
+// start sequencer defines
 #define FIRST_SEQ_STEP      0
 #define LAST_SEQ_STEP       1
 
@@ -53,13 +70,42 @@ oaq_2nd_gen_handle_t algo_handle;
 oaq_2nd_gen_results_t algo_results;
 oaq_2nd_gen_inputs_t algo_input;
 
+void createNewDataFile();
+void writeDataRecordToFile();
 
 void setup()
 {
+
+    pinMode(SD_CS_PIN, OUTPUT);
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
+    pinMode(LED_PIN, OUTPUT);
+    Serial.begin(115200);
+
+    Serial.println("Initializing SD card...");
+    // Configure the SPI pins
+    SPI.setMOSI(PA12);
+    SPI.setMISO(PA6);
+    SPI.setSCLK(PA5);
+    // Start the SPI interface
+    SPI.begin();
+
+    if (!SD.begin(SD_CS_PIN)) {
+        Serial.println("SD card initialization failed!");
+        return;
+    }
+
+    Serial.println("SD card initialization done.");
+
+    // create a new data file
+    createNewDataFile();
+
+    // turn off the LED
+    digitalWrite(LED_PIN, LOW);
+
+
     int8_t lib_ret;
     zmod4xxx_err api_ret;
 
-    Serial.begin(115200);
 
     /**
         Additional delay is required to wait till system is ready.
@@ -70,11 +116,11 @@ void setup()
     Serial.println(F("Starting the Sensor!"));
     /****TARGET SPECIFIC FUNCTION ****/
     /*
-	* To allow the example running on customer-specific hardware, the init_hardware
-	* function must be adapted accordingly. The mandatory funtion pointers *read,
-	* *write and *delay require to be passed to "dev" (reference files located
-	* in "dependencies/zmod4xxx_api/HAL" directory). For more information, read
-	* the Datasheet, section "I2C Interface and Data Transmission Protocol".
+    * To allow the example running on customer-specific hardware, the init_hardware
+    * function must be adapted accordingly. The mandatory funtion pointers *read,
+    * *write and *delay require to be passed to "dev" (reference files located
+    * in "dependencies/zmod4xxx_api/HAL" directory). For more information, read
+    * the Datasheet, section "I2C Interface and Data Transmission Protocol".
     */
     api_ret = init_hardware(&dev);
     if (api_ret) {
@@ -83,7 +129,7 @@ void setup()
         Serial.println(F(" during init hardware, exiting program!\n"));
         error_handle();
     }
-     /****TARGET SPECIFIC FUNCTION ****/
+        /****TARGET SPECIFIC FUNCTION ****/
 
     /* Sensor related data */
     dev.i2c_addr =  ZMOD4450_I2C_ADDR;
@@ -210,15 +256,15 @@ void loop()
     /* get raq control signal and Air Quality Change Rate */
     state = calc_raq( adc_result, &raq_par, &raq_results);
 
+    data[0] = adc_result/1000;
     Serial.println("Measurement:");
-    Serial.println(String("  Rmox:") +String (adc_result/1000,5));
-    // int rmox_kOhm = (int)(adc_result / 1000.0);
-    // sprintf(buffer, "  Rmox  = %5d kOhm\n", rmox_kOhm);
+    Serial.println(String("  Rmox:") +String (data[0],5));
     // Serial.println(buffer);
     if (ZMOD4450_OK == state) {
+        data[1] = raq_results.cs_state;
         Serial.println(String("  raq: control state: ") + String (raq_results.cs_state == ON ? "On" : "Off"));
-        // sprintf(buffer,"  raq: control state %d", raq_results.cs_state);
         // Serial.println(buffer);
+        data[2] = raq_results.conc_ratio;
         Serial.println(String("  raq: Air Quality Change Rate: ") + String (raq_results.conc_ratio,2));
         // sprintf(buffer,"  raq: Air Quality Change Rate %f", raq_results.conc_ratio);
         // Serial.println(buffer);
@@ -240,9 +286,69 @@ void loop()
         }
     }
 
+    // check if the button is pressed
+    if (digitalRead(BUTTON_PIN) == LOW) {
+        // button is pressed, start or stop saving data
+        isSavingData = !isSavingData;
+        if (isSavingData) {
+        Serial.println("Data save started.");
+        digitalWrite(LED_PIN, HIGH);
+        } else {
+        Serial.println("Data save stopped.");
+        digitalWrite(LED_PIN, LOW);
+        dataFile.close();
+        }
+        delay(500); // debounce delay
+    }
+
+    // check if it's time to save data
+    if (isSavingData && millis() - lastDataSaveTime >= DATA_SAVE_INTERVAL) {
+        lastDataSaveTime = millis();
+        // write the data to the file
+        if (dataRecordCount >= FILE_MAX_SIZE) {
+        // maximum file size reached, create a new file
+        createNewDataFile();
+        }
+        writeDataRecordToFile();
+    }
+
 }
 
 void error_handle()
 {
     while (1);
+}
+
+void createNewDataFile() {
+  // close the current file (if open)
+  if (dataFile) {
+    dataFile.close();
+  }
+  // create a new file with a unique name
+  char filename[13];
+  sprintf(filename, "data_%03d.csv", fileIndex);
+  fileIndex++;
+  dataFile = SD.open(filename, FILE_WRITE);
+  if (!dataFile) {
+    Serial.println("Error creating data file!");
+  } else {
+    // write the CSV header
+    dataFile.println("Data1,Data2,Data3,Data4,Data5,Data6,Data7,Data8,Data9,Data10,Data11,Data12");
+    dataRecordCount = 0;
+    Serial.print("New data file created: ");
+    Serial.println(filename);
+  }
+}
+
+void writeDataRecordToFile() {
+
+  // write the data to the file
+  for (int i = 0; i < DATA_REG_SIZE; i++) {
+    dataFile.print(data[i], 2);
+    if (i < DATA_REG_SIZE - 1) {
+      dataFile.print(",");
+    }
+  }
+  dataFile.println();
+  dataRecordCount++;
 }
